c2c7b12c27c38def8f1fc56e05aa87a3903e50a8
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-09-20 22:58:10 +0545

    Raise FileNotFoundError if the specified font isn't available.

--- renderers/renderer.py
+++ renderers/renderer.py
@@ -1,9 +1,14 @@
 from renderers.gimp import gimp_renderer
 from renderers.pygame import pygame_renderer
 
+from pygame.font import match_font
+
 def render(engine, text, textsize, textcolor, font, backgroundcolor, imgwidth, imgheight):
 	"""Render a PNG using the given 'engine', based on the given parameters.
        'engine' can be either 'gimp' or 'pygame'.
     """
+	if match_font(font) == None:
+		raise FileNotFoundError("The specified font '{}' could not be found.".format(font))
+
 	renderer = eval('{}_renderer'.format(engine.lower()))
 	renderer.render(text, textsize, textcolor, font, backgroundcolor, imgwidth, imgheight)

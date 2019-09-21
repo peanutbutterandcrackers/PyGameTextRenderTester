a7029a385bd14d928951411d9b4c6713c90677bf
Prafulla Giri <pratheblackdiamond@gmail.com>
2019-09-21 05:55:43 +0545

    Implement GIMP-renderer-initialization-progress-bar thingy

--- renderers/gimp/gimp_renderer.py
+++ renderers/gimp/gimp_renderer.py
@@ -1,9 +1,15 @@
 import atexit
 import subprocess, shutil
 
+from time import sleep
+
 PLUGIN='pythonfu_gimp_text_renderer.py'
 
-def init():
+# If this module is being called from the context of a package, then
+# show_init_progress; else: let the user choose
+_isPartOfAPackage = __package__ not in ['', None]
+
+def init(show_init_progress):
 	global GIMP_PLUGIN_DIR
 
 	HOME_DIR=shutil.os.path.expanduser('~')
@@ -11,8 +17,25 @@ def init():
 	PLUGIN_PATH=shutil.os.path.join(MODULE_PATH, PLUGIN)
 
 	GIMP_DIRECTORY_LOOKUP_CMD='(python-fu-eval RUN-NONINTERACTIVE "print gimp.directory")'
-	GIMP_DIRECTORY = subprocess.check_output(['gimp-console', '--no-data', '--batch', GIMP_DIRECTORY_LOOKUP_CMD, '--batch', '(gimp-quit 0)'],
-                                             stderr=subprocess.DEVNULL).decode().strip()
+	P = subprocess.Popen(['gimp-console', '--no-data', '--batch', GIMP_DIRECTORY_LOOKUP_CMD, '--batch', '(gimp-quit 0)'],
+                                             stderr=subprocess.DEVNULL, stdout=subprocess.PIPE)
+
+	if show_init_progress:
+		print()
+		progress_marker = '.'
+		progress_str = ''
+		while P.poll() == None:
+			if len(progress_str) > 4:
+				progress_str = progress_str.replace(progress_marker, ' ')
+				print('Initializing GIMP renderer{}\r'.format(progress_str), end='', flush=True)
+				progress_str = ''
+			print('Initializing GIMP renderer{}\r'.format(progress_str), end='', flush=True)
+			progress_str += progress_marker
+			sleep(0.5)
+		print()
+
+	GIMP_DIRECTORY = P.stdout.read(-1).decode().strip()
+
 	GIMP_PLUGIN_DIR=shutil.os.path.join(HOME_DIR, GIMP_DIRECTORY, 'plug-ins')
 
 	shutil.copy2(PLUGIN_PATH, GIMP_PLUGIN_DIR)
@@ -27,5 +50,6 @@ def render(text, textsize, textcolor, font, backgroundcolor, imgwidth, imgheight
 	subprocess.call(['gimp-console', '--no-data', '--batch', GIMP_RENDER_CMD, '--batch', '(gimp-quit 0)']
                                    , stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-atexit.register(stop)
-init()
+if __package__ not in ['', None]:
+	init(show_init_progress=_isPartOfAPackage)
+	atexit.register(stop)
